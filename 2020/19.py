b5bc2c6fa1b12ec71c0ffccc8a57097ebe9f4dc8
Pavel Tetyaev <pahanini@gmail.com>
2020-12-19 18:48:07 +0500

    day 19

---
+++ 2020/19.py
@@ -0,0 +1,30 @@
+from lark import Lark, LarkError
+from utils import read
+import re
+
+
+def pzl1(d):
+    rr, mm = d.split('\n\n')
+    rr = re.sub(r'(\d+)', r'x\1', rr)
+    lark = Lark(rr, start='x0')
+    cnt = 0
+    for m in mm.split():
+        try:
+            lark.parse(m)
+        except LarkError:
+            continue
+        cnt += 1
+    return cnt
+
+
+def pzl2(d):
+    return pzl1(d.replace('8: 42', '8: 42 | 42 8').replace('11: 42 31', '11: 42 31 | 42 11 31'))
+
+
+tst = read('19.tst')
+assert pzl1(tst) == 2
+
+dat = read('19.dat')
+
+print("day 19 puzzle 1 =", pzl1(dat))
+print("day 19 puzzle 2 =", pzl2(dat))
