dc43dd01016bc6975e64dda7a8e6583cbbd7a142
tetyaev <tetyaev@sima-land.ru>
2024-12-02 12:38:33 +0500

    day 1

---
+++ 2024/01.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def pzl1(a):
+    a.sort(axis=0)
+    return np.sum(np.abs(np.diff(a, axis=1)))
+
+
+def pzl2(a):
+    c = np.apply_along_axis(lambda x: np.bincount(x, minlength=100000), 0, a)
+    return sum(np.multiply(np.arange(100000), np.multiply(c[:, 0], c[:, 1])))
+
+
+t = np.loadtxt('01.tst', dtype=int)
+
+assert pzl1(t) == 11
+assert pzl2(t) == 31
+
+d = np.loadtxt('01.dat', dtype=int)
+
+print("day 1 puzzle 1 =", pzl1(d))
+print("day 1 puzzle 2 =", pzl2(d))
